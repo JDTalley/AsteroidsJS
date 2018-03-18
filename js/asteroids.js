177b94b1216409b866705af66c6fb6872946721c
Robert Wenger <wenger.robert.a@gmail.com>
2018-03-18 16:03:25 -0400

    Inital JS

--- js/asteroids.js
+++ js/asteroids.js
@@ -1,1 +1,14 @@
-alert("hi");
+var GAME_FPS = 60;
+
+var msBetweenFrames = 1000 / GAME_FPS;
+var canvas = document.getElementById("game-canvas");
+var context = canvas.msGetInputContext('2d');
+var width = canvas.width;
+var height = canvas.height;
+setTimeout(gameTick, msBetweenFrames);
+
+function gameTick() {
+    // DO STUFF
+
+    setTimeout(gameTick, msBetweenFrames);
+}
