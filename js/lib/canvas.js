bb20e350c2d9d728d8750df5cd240ebf6bbd8d53
Dakota Talley <dakota.talley@gmail.com>
2018-03-18 17:53:39 -0400

    New Branch and Asteroid class

--- js/lib/canvas.js
+++ js/lib/canvas.js
@@ -8,4 +8,18 @@ function Canvas(id) {
         this.context.fillStyle = color;
         this.context.fillRect(0, 0, this.width, this.height);
     }
+
+    this.drawAsteroid = function () {
+        this.context.strokeStyle = "#000000";
+        this.context.lineWidth = 1;
+        this.context.beginPath();
+        for (var i=0; i<9; i++) {
+            ctx.moveTo (Xcenter +  sizex * Math.cos(0), Ycenter +  sizey *  Math.sin(0));          
+        {
+            ctx.lineTo (Xcenter + (sizex + 10 * (Math.floor((Math.random() * 3) + 1))) * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + (sizey + 10 * (Math.floor((Math.random() * 3) + 1))) * Math.sin(i * 2 * Math.PI / numberOfSides));
+        }
+        ctx.lineTo (Xcenter +  sizex * Math.cos(0), Ycenter +  sizey *  Math.sin(0));
+        ctx.stroke();
+        }
+    }
 }
---
+++ js/models/asteroid.js
@@ -0,0 +1,7 @@
+fuction Asteroid(x, y, dx, dy, size) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.size = size;
+}
