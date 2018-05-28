4eacec481b4f7b85de3f7c638eb61e067926f0b8
xSSKT <dakota.talley@gmail.com>
2018-05-28 13:39:01 -0400

    Updated Random Asteroid function. Started random spawn offscreen.

--- js/controllers/asteroids.js
+++ js/controllers/asteroids.js
@@ -1,6 +1,10 @@
 var canvas = new Canvas("game-canvas");
 var spaceship = new Spaceship();
-var asteroid = new Asteroid(100, 100, 0, 0, 3);
+
+var shpSize = randomNumber(1, 3);
+var astLoc = genOutsideWin(shpSize);
+
+var asteroid = new Asteroid(astLoc[0], astLoc[1], 0, 0, shpSize);
 
 var keys = [];
 window.onkeyup = function(e) { keys[e.keyCode] = false; }
@@ -52,6 +56,34 @@ function redrawCanvas() {
     canvas.drawSpaceship("#FFFFFF", spaceship.x, spaceship.y, spaceship.orientation);
     canvas.drawAsteroid(asteroid.points, asteroid.x, asteroid.y);
     //canvas.drawAsteroid(500, 100, 0, 0, 2);
+}
+
+// Return array of x and y outside of the window for spawning
+function genOutsideWin(shpSize){
+    var arr = [];
+    var canvasX = canvas.width;
+    var canvasY = canvas.height;
+    // Left or Right from 0
+    arr[0] = Math.round(Math.random()) * 2 - 1;
+    // Up or Down from 0
+    arr[1] = Math.round(Math.random()) * 2 - 1;
+    // If both are negative, set one lower than screen size
+    if(arr[0] < 1 && arr[1] < 1){
+        Math.random() > 0.5 ? arr[0] = canvasX - shpSize : arr[1] = canvasY - shpSize;
+        return arr;      
+    }
+    //if both are positive, randomize, then pick one and make it larger than screen size
+    if(arr[0] > 1 && arr[0] > 1){
+        arr[0] = Math.random() * canvasX;
+        arr[1] = Math.random() * canvasY;
+        Math.random() > 0.5 ? arr[0] = canvasX + shpSize : arr[1] = canvasY + shpSize;
+        return arr;
+    }
+
+    arr[0] = Math.random() * canvasX;
+    arr[1] = Math.random() * canvasY;
+    arr[0] < 1 ? arr[0] = canvasX - shpSize : arr[1] = canvasY - shpSize;
+    return arr;
 }
 
 var GAME_FPS = 60;
--- js/models/asteroid.js
+++ js/models/asteroid.js
@@ -5,16 +5,23 @@ function Asteroid(x, y, dx, dy, size) {
     this.dy = dy;
     this.size = size;
     this.points = [];
+    this.points[0] = [];
+    this.points[1] = [];
 
-    this.generateShape = function(numberOfSides) {       
-        for (var i = 0; i <= numberOfSides; i += 1) {
-            this.points[i] = [this.x + (this.size + 20 * (Math.floor((Math.random() * 3) + 1))) * Math.cos(i * 2 * Math.PI / numberOfSides),
-            y + (this.size + 20 * (Math.floor((Math.random() * 3) + 1))) * Math.sin(i * 2 * Math.PI / numberOfSides)];
+    this.generateShape = function(numSides) {       
+        for (var i = 0; i <= numSides; i += 1) {
+            this.points[0][i] = this.x + (this.size * 20) * Math.cos(i * 2 * Math.PI / numSides);
+            this.points[1][i] = y + (this.size * 20) * Math.sin(i * 2 * Math.PI / numSides);
         }
-
-        this.points[numberOfSides] = [this.points[0][0], this.points[0][1]];
-
     }
 
-    this.generateShape(10);
+    this.generateShape(randomNumber(5, 10));
+}
+
+// Gets a random number between @param min and @param max
+// * Max is inclusive in this function *
+function randomNumber(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
